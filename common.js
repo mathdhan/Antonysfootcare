43bfd45c94bcd4e3bdd1b0e4cdaa60109a416525
mathdhan <mathewsgeorgemts@gmail.com>
2026-05-18 21:12:08 +0100

    Add files via upload

---
+++ common.js
@@ -0,0 +1,11 @@
+
+<!-- ========================= -->
+<!-- common.js -->
+<!-- ========================= -->
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navbar = document.querySelector('.navbar');
+
+menuToggle.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+});
