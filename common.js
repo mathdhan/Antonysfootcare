405ba340512d2c322d9dcada6fd342bcc2636191
mathdhan <mathewsgeorgemts@gmail.com>
2026-05-19 11:14:39 +0100

    Update common.js

--- common.js
+++ common.js
@@ -9,3 +9,16 @@ const navbar = document.querySelector('.navbar');
 menuToggle.addEventListener('click', () => {
   navbar.classList.toggle('active');
 });
+<script>
+
+const menuToggle = document.getElementById("menu-toggle");
+
+const navbar = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+
+  navbar.classList.toggle("active");
+
+});
+
+</script>
